d345da3325da0c0eef563d5895e030fe7d4b3e1e
Cameron Taggart <cameron@tachyus.com>
2015-12-27 23:14:09 -0800

    started hacking a new Edge test for TsAst#5

--- test/MsieJavaScriptEngine.Test.ChakraEdgeJsRt/CommonTests.cs
+++ test/MsieJavaScriptEngine.Test.ChakraEdgeJsRt/CommonTests.cs
@@ -19,5 +19,28 @@
 
 			return jsEngine;
 		}
-	}
+
+        //var person = { firstName:"John", lastName:"Doe", age:50, eyeColor:"blue" };
+        [Test]
+        public virtual void PassJsRefToFunction()
+        {
+            // Arrange
+            const string input = "'Hello, ' + \"Vasya\" + '?';";
+            const string targetOutput = "Hello, Vasya?";
+
+            // Act
+            string output;
+
+            using (var jsEngine = CreateJsEngine())
+            {
+                jsEngine.Execute(@"function create(){ return { firstName: ""John"", lastName: ""Doe"", age: 50, eyeColor: ""blue"" } };");
+                var person = jsEngine.CallFunction("create");
+                System.Diagnostics.Debug.WriteLine("person is " + person);
+                output = jsEngine.Evaluate<string>(input);
+            }
+
+            // Assert
+            Assert.AreEqual(targetOutput, output);
+        }
+    }
 }
